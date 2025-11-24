80a43d766e7e0e905753d58c02455540deaf103b
André Zana <andrezana@hotmail.com>
2025-11-24 11:36:48 -0300

    primeiros passos com python

---
+++ magica.py
@@ -0,0 +1,22 @@
+import os #Importando a biblioteca OS (Chamada de funções do sistema operacional)
+
+os.system('cls') #Comando para limpar o terminal (cls)
+
+print('Olhe as cartas abaixo e escolha uma:')
+print('-----')
+print('Q♥️ J♠️ J♦️ K♣️ A♣️ Q♦️')
+input() #Input é onde o "user" deve inserir algo, mas como é uma mágica, pressiono enter e deixo o resto da mágica acontecer.
+#O input está vazio por este motivo
+
+os.system('cls')
+print('A carta que você escolheu...')
+input()
+
+print()
+print('DE SA PA RE CEU!')
+
+print()
+print('J♣️ K♦️ A♠️ K♠️ J♥️ A♦️')
+
+print()
+print('Sumiu! RSRSRSRS')
---
+++ programa.py
@@ -0,0 +1,6 @@
+print("Hello, World!")
+print("Ele processa cada print")
+print(7 * 7)
+
+print('Ele processa cada print - TESTE')
+print("Funciona com aspas duplas e aspas simples")
