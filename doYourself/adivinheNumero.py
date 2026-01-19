9615738cd0f03cf3272e0ed098886a7f0e1343f0
André Zana <andrezana@hotmail.com>
2026-01-19 12:12:59 -0300

    Create adivinheNumero.py

---
+++ doYourself/adivinheNumero.py
@@ -0,0 +1,13 @@
+import os
+
+os.system('cls')
+
+print('Pense em um número de 1 à 10!')
+input('Aperte "ENTER" após escolher o número!')
+
+os.system('cls')
+print('O número escolhido é... ')
+input()
+print(10)
+print()
+print('Acertei?! kkkk 😁')
