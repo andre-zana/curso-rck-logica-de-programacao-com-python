4423ac8b84e83f57d376070be845cffc8534ed8f
André Zana <andrezana@hotmail.com>
2026-01-19 12:13:05 -0300

    Create penseEmUmaCor.py

---
+++ doYourself/penseEmUmaCor.py
@@ -0,0 +1,34 @@
+import os #Importando a biblioteca OS (Chamada de funções do sistema operacional)
+
+os.system('cls') #Comando para limpar o terminal (cls)
+
+print('Pense em uma cor.')
+print('🔴 Vermelho')
+print('🟠 Laranja')
+print('🟡 Amarelo')
+print('🟢 Verde')
+print('🔵 Azul')
+print('Pensou?!?')
+input('Se sim, então aperte "ENTER"!')
+
+os.system('cls')
+print('Agora, vou pedir para você relaxar, ficar tranquila e calma, enquanto eu me concentro, entro em sua mente e descobro à cor escolhida. 😂 kkkk')
+input()
+
+print('Calma lá...')
+input()
+
+print('Mais um pouquinho...')
+input()
+
+print('Só mais um pouquinho...')
+input()
+
+print('Estou quase...')
+input()
+
+print('Ahá!')
+print('DESCOBRI!')
+input()
+
+print('Você pensou em AMARELO. Acertei?!?')
