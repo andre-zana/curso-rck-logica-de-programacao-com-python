cc4056f667d57ba6041300845dfd7dadc19e1095
André Zana <andrezana@hotmail.com>
2026-01-19 12:13:07 -0300

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,18 @@
+print("Hello, World!")
+print("Ele processa cada print")
+print(7 * 7)
+
+print('Ele processa cada print - TESTE')
+print("Funciona com aspas duplas e aspas simples")
+
+#----- Continuação em outro vídeo
+
+x = 10 # X recebe 10 e não, X é igual a 10!
+print(x * 2)
+print(x * 5)
+
+# Tipos de Dados:
+# numéricos, texto, booleano (lógico).
+
+# Algumas linguagens de programação são estaticamente tipadas, ou seja, é necessário declarar os tipos de dados antes de usar.
+# Python é dinamicamente tipada, sendo assim, não precisa declarar o tipo de dado.
